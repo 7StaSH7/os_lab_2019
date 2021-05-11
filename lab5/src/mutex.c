8681852b251b1c4131b9b4a102e4b789378f3648
7stash7 <sanchasyo@mail.ru>
2021-05-11 09:03:28 +0000

    Did half of lab5

--- lab5/src/mutex.c
+++ lab5/src/mutex.c
@@ -62,8 +62,7 @@ void do_one_thing(int *pnum_times) {
     work = *pnum_times;
     printf("counter = %d\n", work);
     work++; /* increment, but not write */
-    for (k = 0; k < 500000; k++)
-      ;                 /* long cycle */
+    for (k = 0; k < 500000; k++); /* long cycle */
     *pnum_times = work; /* write back */
 	// pthread_mutex_unlock(&mut);
   }
@@ -79,8 +78,7 @@ void do_another_thing(int *pnum_times) {
     work = *pnum_times;
     printf("counter = %d\n", work);
     work++; /* increment, but not write */
-    for (k = 0; k < 500000; k++)
-      ;                 /* long cycle */
+    for (k = 0; k < 500000; k++);   /* long cycle */
     *pnum_times = work; /* write back */
     // pthread_mutex_unlock(&mut);
   }
