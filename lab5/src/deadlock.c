bde2e19baeec513db4fa81e811035559795296bc
7stash7 <sanchasyo@mail.ru>
2021-05-25 06:55:21 +0000

    Done lab 5

--- lab5/src/deadlock.c
+++ lab5/src/deadlock.c
@@ -32,13 +32,17 @@ void *deadlock_first(void *args) {
 int main() {
 
   pthread_t threads[3];
+
   pthread_mutex_init(&first_variable_mutex, NULL);
   pthread_mutex_init(&second_variable_mutex, NULL);
   pthread_mutex_init(&third_variable_mutex, NULL); 
+
   pthread_create(&threads[0], NULL, deadlock_first, NULL);
   pthread_create(&threads[1], NULL, deadlock_first, NULL);
-  pthread_create(&threads[2], NULL, deadlock_first, NULL);   
+  pthread_create(&threads[2], NULL, deadlock_first, NULL);
+
   for (int i = 1; i < 3; i++) pthread_join(threads[i], NULL);
   printf("First - %i\nSecond - %i\nThird - %i\n", first_variable, second_variable, third_variable);
+  
   return 0;
  }
